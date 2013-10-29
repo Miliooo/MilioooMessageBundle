23886829f4373c2e641d21d8e6ea880520fce1a0
michiel <boeckaert@gmail.com>
2013-10-29 02:17:34 +0100

    updates

--- MessagingBundle/Entity/MessageMeta.php
+++ MessagingBundle/Entity/MessageMeta.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of the MilioooMessageBundle package.
- *
+ * 
  * (c) Michiel boeckaert <boeckaert@gmail.com>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
@@ -10,14 +10,14 @@
 
 namespace Miliooo\MessagingBundle\Entity;
 
-use Miliooo\Messaging\Model\Message as ModelMessage;
+use Miliooo\Messaging\Model\MessageMeta as ModelMessageMeta;
 
 /**
- * Abstract Message class
+ * Abstract class messagemeta
  *
  * @author Michiel Boeckaert <boeckaert@gmail.com>
  */
-abstract class Message extends ModelMessage
+abstract class MessageMeta extends ModelMessageMeta
 {
 
 }
--- MessagingBundle/Entity/ThreadMeta.php
+++ MessagingBundle/Entity/ThreadMeta.php
@@ -2,7 +2,7 @@
 
 /*
  * This file is part of the MilioooMessageBundle package.
- *
+ * 
  * (c) Michiel boeckaert <boeckaert@gmail.com>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
@@ -10,14 +10,14 @@
 
 namespace Miliooo\MessagingBundle\Entity;
 
-use Miliooo\Messaging\Model\Message as ModelMessage;
+use Miliooo\Messaging\Model\ThreadMeta as ModelThreadMeta;
 
 /**
- * Abstract Message class
+ * Description of ThreadMeta
  *
  * @author Michiel Boeckaert <boeckaert@gmail.com>
  */
-abstract class Message extends ModelMessage
+abstract class ThreadMeta extends ModelThreadMeta
 {
 
 }
