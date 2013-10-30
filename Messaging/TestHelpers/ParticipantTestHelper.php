5ab0e72fa5c731315c709e0ed0fcb1fe33c9a751
michiel <boeckaert@gmail.com>
2013-10-30 10:54:32 +0100

    Test file for participantTestHelper

--- Messaging/TestHelpers/ParticipantTestHelper.php
+++ Messaging/TestHelpers/ParticipantTestHelper.php
@@ -13,7 +13,10 @@ namespace Miliooo\Messaging\TestHelpers;
 use Miliooo\Messaging\User\ParticipantInterface;
 
 /**
- * Description of ParticipantTest
+ * Helper class to create new participants for tests
+ *
+ * The constructor argument $participantId is what we return in getParticipantId()
+ * This makes it easy to create unique participants
  *
  * @author Michiel Boeckaert <boeckaert@gmail.com>
  */
@@ -21,11 +24,19 @@ class ParticipantTestHelper implements ParticipantInterface
 {
     protected $participantId;
 
+    /**
+     * Constructor.
+     *
+     * @param string $participantId
+     */
     public function __construct($participantId)
     {
         $this->participantId = $participantId;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getParticipantId()
     {
         return $this->participantId;
---
+++ Messaging/Tests/TestHelpers/ParticipantTestHelperTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the MilioooMessageBundle package.
+ *
+ * (c) Michiel boeckaert <boeckaert@gmail.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Miliooo\Messaging\Tests\TestHelpers;
+
+use Miliooo\Messaging\TestHelpers\ParticipantTestHelper;
+
+/**
+ * Test file for Miliooo\Messaging\TestHelpers\ParticipantTestHelper
+ *
+ * @author Michiel Boeckaert <boeckaert@gmail.com>
+ */
+class ParticipantTestHelperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInstanceOfParticipantInterface()
+    {
+        $class = new ParticipantTestHelper(1);
+        $this->assertInstanceOf('Miliooo\Messaging\User\ParticipantInterface', $class);
+    }
+
+    public function testConstructorArgumentIsGetParticipantIdResult()
+    {
+        $class1 = new ParticipantTestHelper(1);
+        $this->assertEquals(1, $class1->getParticipantId());
+
+        $class2 = new ParticipantTestHelper('foo');
+        $this->assertEquals('foo', $class2->getParticipantId());
+    }
+}
