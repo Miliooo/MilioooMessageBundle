35c126d9d94182b918426438b5f252e416cfb30f
michiel <boeckaert@gmail.com>
2013-11-02 19:59:00 +0100

    test file for specification CanSeeThread

---
+++ Messaging/Tests/Specifications/CanSeeThreadSpecificationTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the MilioooMessageBundle package.
+ *
+ * (c) Michiel boeckaert <boeckaert@gmail.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Miliooo\MessagingBundle\Tests\Specifications;
+
+use Miliooo\Messaging\Specifications\CanSeeThreadSpecification;
+use Miliooo\Messaging\TestHelpers\ParticipantTestHelper;
+
+/**
+ * Test file for Miliooo\Messaging\Specifications\CanSeeThreadSpecification
+ *
+ * @author Michiel Boeckaert <boeckaert@gmail.com>
+ */
+class CanSeeThreadSpecificationTest extends \PHPUnit_Framework_TestCase
+{
+    private $specification;
+    private $thread;
+    private $participant;
+
+    public function setUp()
+    {
+        $this->thread = $this->getMock('Miliooo\Messaging\Model\ThreadInterface');
+        $this->participant = new ParticipantTestHelper(1);
+        $this->specification = new CanSeeThreadSpecification;
+    }
+
+    public function testIsSatisfiedByReturnsTrueWhenParticipant()
+    {
+        $this->thread->expects($this->once())->method('isParticipant')
+            ->with($this->participant)->will($this->returnValue(true));
+        $this->assertTrue($this->specification->isSatisfiedBy($this->participant, $this->thread));
+    }
+
+    public function testIsSatisfiedByReturnsFalseWhenNotParticipant()
+    {
+        $this->thread->expects($this->once())->method('isParticipant')
+            ->with($this->participant)->will($this->returnValue(false));
+        $this->assertFalse($this->specification->isSatisfiedBy($this->participant, $this->thread));
+    }
+}
