4a98d5776f0dcc2321f70e511204b8e61f3837d8
michiel <boeckaert@gmail.com>
2013-11-02 19:58:02 +0100

    Updated docs in canseethreadspecification

--- Messaging/Specifications/CanSeeThreadSpecification.php
+++ Messaging/Specifications/CanSeeThreadSpecification.php
@@ -14,12 +14,30 @@ use Miliooo\Messaging\User\ParticipantInterface;
 use Miliooo\Messaging\Model\ThreadInterface;
 
 /**
- * Description of CanSeeThreadSpecification
+ * CanSeeThreadSpecification.
+ *
+ * The can see thread specification is responsible for deciding if the given
+ * participant can see the given thread.
+ *
+ * The default implementation is that they have to be participant of that thread.
+ * Since it would not make much sense to not let the participant see that thread
+ *
+ * One of the many reasons to  extend could be to check if the participant
+ * has a certain role... (eg ROLE_SUPER_ADMIN)
  *
  * @author Michiel Boeckaert <boeckaert@gmail.com>
  */
 class CanSeeThreadSpecification
 {
+
+    /**
+     * Checks if the given participant can see the given thread
+     *
+     * @param ParticipantInterface $participant The user that we check for
+     * @param ThreadInterface      $thread      The thread that we check
+     *
+     * @return boolean true if participant (user) can see the thread, false otherwise
+     */
     public function isSatisfiedBy(ParticipantInterface $participant, ThreadInterface $thread)
     {
         if ($thread->isParticipant($participant)) {
