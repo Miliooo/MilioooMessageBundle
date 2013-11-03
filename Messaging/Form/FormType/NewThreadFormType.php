2f1be1df250341efbc34976064b5525c48cb04e6
michiel <boeckaert@gmail.com>
2013-11-03 20:53:47 +0100

    updated newthreadformtype and added missing test file

--- Messaging/Form/FormType/NewThreadFormType.php
+++ Messaging/Form/FormType/NewThreadFormType.php
@@ -15,13 +15,32 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
- * Message form type for starting a new conversation
+ * Form type for starting a new thread
+ *
+ * Notice this form type does not supply all the required fields for the form model.
+ * This is because some settings gets set without the form.
+ *
+ * In the form factory we set the sender of a new thread. (by the controller)
+ * In the form handler we set the creation date of the new thread and message
+ * That's because the datetime is the datetime the form is submitted.
  *
  * @author Michiel Boeckaert <boeckaert@gmail.com>
  */
 class NewThreadFormType extends AbstractType
 {
 
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting form the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array                $options The options
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     *
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -30,13 +49,23 @@ class NewThreadFormType extends AbstractType
             ->add('body', 'textarea');
     }
 
+    /**
+     * Sets the default options for this type.
+     *
+     * @param OptionsResolverInterface $resolver The resolver for the options.
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'intention' => 'message',
+            'intention' => 'add_new_thread',
         ));
     }
 
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
     public function getName()
     {
         return 'miliooo_message_new_thread';
---
+++ Messaging/Tests/Form/FormType/NewThreadFormTypeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the MilioooMessageBundle package.
+ *
+ * (c) Michiel boeckaert <boeckaert@gmail.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Miliooo\Messaging\Tests\Form\FormType;
+
+use Miliooo\Messaging\Form\FormType\NewThreadFormType;
+
+/**
+ * Test file for Miliooo\Messaging\Form\FormType\NewThreadFormType
+ *
+ * This is a basic test we should check
+ * http://symfony.com/doc/master/cookbook/form/unit_testing.html
+ *
+ * @author Michiel Boeckaert <boeckaert@gmail.com>
+ */
+class NewThreadFormTypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * The class under test
+     * @var NewThreadFormType
+     */
+    private $newThreadFormType;
+
+    public function setUp()
+    {
+        $this->newThreadFormType = new NewThreadFormType;
+    }
+
+    public function testInterface()
+    {
+        $this->assertInstanceOf('Symfony\Component\Form\FormTypeInterface', $this->newThreadFormType);
+    }
+
+    public function testBuildForm()
+    {
+        $formBuilder = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
+                ->disableOriginalConstructor()->getMock();
+        $formBuilder->expects($this->at(0))->method('add')->with('recipients', 'username_selector')
+            ->will($this->returnValue($formBuilder));
+        $formBuilder->expects($this->at(1))->method('add')->with('subject', 'text')
+            ->will($this->returnValue($formBuilder));
+        $formBuilder->expects($this->at(2))->method('add')->with('body', 'textarea')
+            ->will($this->returnValue($formBuilder));
+
+        $this->newThreadFormType->buildForm($formBuilder, array());
+    }
+
+    public function testSetDefaultOptions()
+    {
+        $defaults = ['intention' => 'add_new_thread'];
+        $resolver = $this->getMock('Symfony\Component\OptionsResolver\OptionsResolverInterface');
+        $resolver->expects($this->once())->method('setDefaults')->with($defaults);
+        $this->newThreadFormType->setDefaultOptions($resolver);
+    }
+
+    public function testGetName()
+    {
+        $this->assertSame('miliooo_message_new_thread', $this->newThreadFormType->getName());
+    }
+}
