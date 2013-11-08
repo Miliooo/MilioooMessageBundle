72b89a4320b584cd10fd3473615f4e49178b20cf
michiel <boeckaert@gmail.com>
2013-11-08 21:11:40 +0100

    try to add repository class in config but think it will be needed to be added in custom user classes

---
+++ MessagingBundle/Entity/ThreadRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the MilioooMessageBundle package.
+ *
+ * (c) Michiel boeckaert <boeckaert@gmail.com>
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+namespace Miliooo\MessagingBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Description of ThreadRepository
+ *
+ * @author Michiel Boeckaert <boeckaert@gmail.com>
+ */
+class ThreadRepository extends EntityRepository
+{
+    //put your code here
+}
