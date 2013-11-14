830ef62365953e031546b04c600cc32520a7ebe1
michiel <boeckaert@gmail.com>
2013-11-14 17:56:25 +0100

    cs

--- Messaging/Event/ReadStatusMessageEventInterface.php
+++ Messaging/Event/ReadStatusMessageEventInterface.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Miliooo\Messaging\Event;
-
 /*
  * This file is part of the MilioooMessageBundle package.
  *
@@ -9,6 +7,8 @@ namespace Miliooo\Messaging\Event;
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
+namespace Miliooo\Messaging\Event;
 
 use Miliooo\Messaging\Model\MessageInterface;
 use Miliooo\Messaging\Model\ThreadInterface;
