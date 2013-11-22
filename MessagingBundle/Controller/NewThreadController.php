686f6bd8a47e2489f80ef6be9a8b7c239b20f9bc
michiel <boeckaert@gmail.com>
2013-11-22 04:02:37 +0100

    updated the new thread controller to redirect and add a flash when submitting a new thread

--- MessagingBundle/Controller/NewThreadController.php
+++ MessagingBundle/Controller/NewThreadController.php
@@ -15,6 +15,9 @@ use Miliooo\Messaging\Form\FormHandler\NewSingleThreadFormHandler;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Miliooo\Messaging\User\ParticipantProviderInterface;
+use Miliooo\Messaging\Helpers\FlashMessages\FlashMessageProviderInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Controller for adding new threads.
@@ -25,29 +28,73 @@ use Miliooo\Messaging\User\ParticipantProviderInterface;
  */
 class NewThreadController
 {
+    /**
+     * A new thread message form factory instance.
+     *
+     * @var NewThreadMessageFormFactory
+     */
     protected $formFactory;
+
+    /**
+     * A new single thread form handler.
+     *
+     * @var NewSingleThreadFormHandler
+     */
     protected $formHandler;
+
+    /**
+     * A participant provider.
+     *
+     * @var ParticipantProviderInterface
+     */
     protected $participantProvider;
+
+    /**
+     * An templating instance.
+     *
+     * @var EngineInterface
+     */
     protected $templating;
+
+    /**
+     * A flash message provider.
+     *
+     * @var FlashMessageProviderInterface
+     */
+    protected $flashMessageProvider;
+
+    /**
+     * A router instance.
+     *
+     * @var RouterInterface
+     */
+    protected $router;
 
     /**
      * Constructor.
      *
-     * @param NewThreadMessageFormFactory  $formFactory         A formfactory instance for a new thread
-     * @param NewSingleThreadFormHandler   $formHandler         A new single thread formhandler instance
-     * @param ParticipantProviderInterface $participantProvider A participant provider
-     * @param EngineInterface              $templating          A templating engine instance
+     * @param NewThreadMessageFormFactory   $formFactory          A formfactory instance for a new thread
+     * @param NewSingleThreadFormHandler    $formHandler          A new single thread formhandler instance
+     * @param ParticipantProviderInterface  $participantProvider  A participant provider
+     * @param EngineInterface               $templating           A templating engine instance
+     * @param FlashMessageProviderInterface $flashMessageProvider A flash message provider instance
+     * @param RouterInterface               $router               A router instance
      */
     public function __construct(
         NewThreadMessageFormFactory $formFactory,
         NewSingleThreadFormHandler $formHandler,
         ParticipantProviderInterface $participantProvider,
-        EngineInterface $templating
+        EngineInterface $templating,
+        FlashMessageProviderInterface $flashMessageProvider,
+        RouterInterface $router
         ) {
         $this->formFactory = $formFactory;
         $this->formHandler = $formHandler;
         $this->participantProvider = $participantProvider;
         $this->templating = $templating;
+        $this->flashMessageProvider = $flashMessageProvider;
+        $this->router = $router;
+
     }
 
     /**
@@ -63,9 +110,33 @@ class NewThreadController
     {
         $sender = $this->participantProvider->getAuthenticatedParticipant();
         $form = $this->formFactory->create($sender);
-        $this->formHandler->process($form);
+        $processed = $this->formHandler->process($form);
+        if ($processed) {
+            return $this->doProcessValidForm();
+        }
+
         $twig = 'MilioooMessagingBundle:NewThread:new_thread.html.twig';
 
-        return $this->templating->renderResponse($twig, array('form' => $form->createView()));
+        return $this->templating->renderResponse($twig, ['form' => $form->createView()]);
+    }
+
+    /**
+     * Processes a valid form.
+     *
+     * Here we process a valid submitted form.
+     * We add a flash message and redirect the user to the new thread page.
+     *
+     * @return RedirectResponse
+     */
+    protected function doProcessValidForm()
+    {
+        $this->flashMessageProvider->addFlash(
+            FlashMessageProviderInterface::TYPE_SUCCESS,
+            'thread_created_success'
+        );
+
+        $url = $this->router->generate('miliooo_message_thread_new');
+
+        return new RedirectResponse($url);
     }
 }
--- MessagingBundle/Tests/Controller/NewThreadControllerTest.php
+++ MessagingBundle/Tests/Controller/NewThreadControllerTest.php
@@ -10,8 +10,10 @@
 
 namespace Miliooo\MessagingBundle\Tests\Controller;
 
+use Miliooo\Messaging\User\ParticipantInterface;
 use Miliooo\MessagingBundle\Controller\NewThreadController;
 use Miliooo\Messaging\TestHelpers\ParticipantTestHelper;
+use Miliooo\Messaging\Helpers\FlashMessages\FlashMessageProviderInterface;
 
 /**
  * Test file for Miliooo\MessagingBundle\Controller\NewThreadController
@@ -26,41 +28,124 @@ class NewThreadControllerTest extends \PHPUnit_Framework_TestCase
      * @var NewThreadController
      */
     private $controller;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
     private $formFactory;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
     private $formHandler;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
     private $participantProvider;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
     private $templating;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $flashMessageProvider;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $router;
+
+    /**
+     * @var ParticipantInterface
+     */
+    private $loggedInUser;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $form;
 
     public function setUp()
     {
-        $this->formFactory = $this->getMockBuilder('Miliooo\Messaging\Form\FormFactory\NewThreadMessageFormFactory')->disableOriginalConstructor()->getMock();
-        $this->formHandler = $this->getMockBuilder('Miliooo\Messaging\Form\FormHandler\NewSingleThreadFormHandler')->disableOriginalConstructor()->getMock();
+        $this->formFactory = $this->getMockBuilder('Miliooo\Messaging\Form\FormFactory\NewThreadMessageFormFactory')
+            ->disableOriginalConstructor()->getMock();
+        $this->formHandler = $this->getMockBuilder('Miliooo\Messaging\Form\FormHandler\NewSingleThreadFormHandler')
+            ->disableOriginalConstructor()->getMock();
         $this->participantProvider = $this->getMock('Miliooo\Messaging\User\ParticipantProviderInterface');
         $this->templating = $this->getMock('Symfony\Bundle\FrameworkBundle\Templating\EngineInterface');
-        $this->controller = new NewThreadController($this->formFactory, $this->formHandler, $this->participantProvider, $this->templating);
+        $this->flashMessageProvider = $this->getMock(
+            'Miliooo\Messaging\Helpers\FlashMessages\FlashMessageProviderInterface'
+        );
+        $this->router = $this->getMock('Symfony\Component\Routing\RouterInterface');
+        $this->loggedInUser = new ParticipantTestHelper('1');
+        $this->form = $this->getMockBuilder('Symfony\Component\Form\Form')->disableOriginalConstructor()->getMock();
+        $this->controller = new NewThreadController(
+            $this->formFactory,
+            $this->formHandler,
+            $this->participantProvider,
+            $this->templating,
+            $this->flashMessageProvider,
+            $this->router
+        );
     }
 
-    public function testCreateAction()
+    public function testCreateActionWithNonProcessedForm()
     {
-        $form = $this->getMockBuilder('Symfony\Component\Form\Form')->disableOriginalConstructor()->getMock();
-        $sender = new ParticipantTestHelper('sender');
+        $this->expectsLoggedInUser();
+        $this->expectsFormProcessingWillReturn(false);
+        $this->expectsTwigRendering();
+        $this->flashMessageProvider->expects($this->never())->method('addFlash');
+        $this->controller->createAction();
+    }
 
+    public function testCreateActionWithProcessedForm()
+    {
+        $this->expectsLoggedInUser();
+        $this->expectsFormProcessingWillReturn(true);
+        $this->flashMessageProvider->expects($this->once())->method('addFlash')
+            ->with(FlashMessageProviderInterface::TYPE_SUCCESS, 'thread_created_success');
+        $this->router->expects($this->once())
+            ->method('generate')
+            ->with('miliooo_message_thread_new')
+            ->will($this->returnValue('http://www.test.com'));
+
+        $this->controller->createAction();
+    }
+
+    protected function expectsLoggedInUser()
+    {
         $this->participantProvider->expects($this->once())
-            ->method('getAuthenticatedParticipant')->will($this->returnValue($sender));
+            ->method('getAuthenticatedParticipant')->will($this->returnValue($this->loggedInUser));
+    }
 
+    /**
+     * Expects Form processing and the process method will return the first parameter value.
+     *
+     * @param boolean $boolean
+     */
+    protected function expectsFormProcessingWillReturn($boolean)
+    {
         $this->formFactory->expects($this->once())
             ->method('create')
-            ->with($sender)
-            ->will($this->returnValue($form));
-        $this->formHandler->expects($this->once())->method('process')->with($form);
+            ->with($this->loggedInUser)
+            ->will($this->returnValue($this->form));
 
+        $this->formHandler->expects($this->once())->method('process')->with($this->form)
+            ->will($this->returnValue($boolean));
+    }
+
+    protected function expectsTwigRendering()
+    {
         $formView = $this->getMockBuilder('Symfony\Component\Form\FormView')->disableOriginalConstructor()->getMock();
-        $form->expects($this->once())->method('createView')->will($this->returnValue($formView));
+        $this->form->expects($this->once())->method('createView')->will($this->returnValue($formView));
 
         $this->templating
             ->expects($this->once())
             ->method('renderResponse')
-            ->with('MilioooMessagingBundle:NewThread:new_thread.html.twig', array('form' => $formView));
-        $this->controller->createAction();
+            ->with('MilioooMessagingBundle:NewThread:new_thread.html.twig', ['form' => $formView]);
     }
 }
