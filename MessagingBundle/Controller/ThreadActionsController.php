8dfadae93c05f69d9e7cc07f5f30fccc607c32ec
michiel <boeckaert@gmail.com>
2013-11-22 07:35:29 +0100

    thread actions controller update

--- MessagingBundle/Controller/ThreadActionsController.php
+++ MessagingBundle/Controller/ThreadActionsController.php
@@ -20,12 +20,67 @@ use Symfony\Component\HttpFoundation\Request;
 class ThreadActionsController
 {
     /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
      * @param Request $request The request
      */
     public function threadAction(Request $request)
     {
-        $data = $request->query->all();
-        var_dump($data);
+        $threadIds = $this->getThreadIds($request);
+        $threadAction = $this->getThreadAction($request);
+
+        //no action needed here... probably no threads selected but pressed submit
+        if (!is_array($threadIds) || !$threadAction) {
+            exit('invalid arguments');
+        }
+
+
+        var_dump($threadIds);
+        var_dump($threadAction);
+
+
         exit('test');
+    }
+
+    /**
+     * Gets the thread ids from the request.
+     *
+     * @param Request $request
+     *
+     * @return array|false An array with thread ids selected or false when no thread id array given.
+     */
+
+    protected function getThreadIds(Request $request)
+    {
+        $threadIds = $request->request->get('selected_threads');
+
+        if (empty($threadIds) || !is_array($threadIds)) {
+            return false;
+        }
+
+        return $threadIds;
+    }
+
+    /**
+     * Gets the thread action from the request.
+     *
+     * @param Request $request
+     *
+     * @return string|false
+     */
+    protected function getThreadAction(Request $request)
+    {
+        $threadAction = $request->request->get('thread_action');
+        if (empty($threadAction) || !is_string($threadAction)) {
+            return false;
+        }
+
+        return $threadAction;
     }
 }
